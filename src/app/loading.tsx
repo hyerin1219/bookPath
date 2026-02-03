205c01185518c4bfd9f9abfd4f0803e339594cec
hyerin <gpfls662@gmail.com>
2026-02-03 16:56:48 +0900

    feat'; loading.tsx 추가

---
+++ src/app/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+    return (
+        <div className="flex items-center justify-center min-h-screen">
+            <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
+            <p className="ml-3">데이터를 불러오는 중입니다...</p>
+        </div>
+    );
+}
